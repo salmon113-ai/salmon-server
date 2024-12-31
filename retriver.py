5f0075d2bb26cd0e67a00404d14e10e10d2038b6
hajubal <hajubal@gmail.com>
2024-12-31 11:55:58 +0900

    Config: langfuse, langchain-ollama 의존성 추가

--- retriver.py
+++ retriver.py
@@ -1,8 +1,7 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_community.vectorstores import FAISS
-from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
-import os
+from langchain_ollama import OllamaEmbeddings
 
 def create_retriever(file_path):
     # 단계 1: 문서 로드(Load Documents)
@@ -14,15 +13,11 @@ def create_retriever(file_path):
     split_documents = text_splitter.split_documents(docs)
 
     # 단계 3: 임베딩(Embedding) 생성
-    embeddings = HuggingFaceEndpointEmbeddings(
-        model="intfloat/multilingual-e5-large-instruct",
-        task="feature-extraction",
-        huggingfacehub_api_token=os.environ["HUGGINGFACEHUB_API_TOKEN"],
-    )
+    embeddings_model = OllamaEmbeddings(model="bge-m3")
 
     # 단계 4: DB 생성(Create DB) 및 저장
     # 벡터스토어를 생성합니다.
-    vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings)
+    vectorstore = FAISS.from_documents(documents=split_documents, embedding=embeddings_model)
 
     # 단계 5: 검색기(Retriever) 생성
     # 문서에 포함되어 있는 정보를 검색하고 생성합니다.
